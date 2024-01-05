062e6100f29450a6111c55f4f1a23e9a37c6deb8
Yehor Ivashkin <ivashin.egor98@gmail.com>
2024-01-05 18:33:49 +0200

    +++

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,21 @@
 const cards = document.querySelectorAll('.card__item');
 
 cards.forEach((card) => {
-  const childCard = card.querySelector('.card__btns');
-
   card.addEventListener('click', (e) => {
     const target = e.currentTarget;
-
     cards.forEach((otherCard) => {
       if (otherCard !== target) {
         otherCard.classList.remove('active');
-      }
-
-      if (otherCard.classList.contains('active')) {
-        childCard.classList.remove('visibility-off');
+        const otherCardBtns = otherCard.querySelector('.card__btns');
+        if (otherCardBtns && !otherCard.classList.contains('active')) {
+          otherCardBtns.classList.add('visibility-off');
+        }
       }
     });
-
-    childCard.classList.add('visibility-off');
     target.classList.add('active');
+    const targetBtns = target.querySelector('.card__btns');
+    if (targetBtns) {
+      targetBtns.classList.remove('visibility-off');
+    }
   });
 });
